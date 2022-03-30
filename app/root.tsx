9ddb4f150208cfad6325dc28ada57514c85f9257
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-03-29 23:22:33 -0700

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,36 @@ export default function App() {
       <head>
         <Meta />
         <Links />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/favicon/safari-pinned-tab.svg"
+          color="#555555"
+        />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta
+          name="msapplication-config"
+          content="/favicon/browserconfig.xml"
+        />
+        <meta name="theme-color" content="#ffffff" />
       </head>
       <body className="bg-slate-900 text-white bg-gradient-to-br from-slate-800 via-slate-900 to-black min-h-screen">
         <Outlet />

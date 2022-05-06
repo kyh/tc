c11c42d09978ef7512f4c634f5e0fb2340cd5b26
Kaiyu Hsu <im.kaiyu@gmail.com>
2022-05-05 22:53:07 -0700

    add all sections

--- app/components/CompModal.tsx
+++ app/components/CompModal.tsx
@@ -17,6 +17,10 @@ export const CompModal = ({
   shareType,
   shareCalcType,
 }: Props) => {
+  const isoCurrent = shareType === "iso" && shareCalcType == "current";
+  const rsuCurrent = shareType === "rsu" && shareCalcType == "current";
+  const isoRevenue = shareType === "iso" && shareCalcType == "revenue";
+  const rsuRevenue = shareType === "rsu" && shareCalcType == "revenue";
   return (
     <Modal
       isOpen={isOpen}
@@ -24,55 +28,110 @@ export const CompModal = ({
       closeModal={closeModal}
       title="Terminology"
     >
+      {isoCurrent && (
+        <div className="mt-3">
+          <h2 className="text-slate-50 font-bold">Preferred Stock Price</h2>
+          <p className="mt-1">
+            The preferred stock price is the price at which investors currently
+            pay for shares of the company. You can ask your recruiter what the
+            current price is.
+          </p>
+        </div>
+      )}
+      {rsuCurrent && (
+        <div className="mt-3">
+          <h2 className="text-slate-50 font-bold">Current Market Price</h2>
+          <p className="mt-1">
+            The current market price is the price at which the company on the
+            market.
+          </p>
+        </div>
+      )}
+      {(isoRevenue || rsuRevenue) && (
+        <div className="mt-3">
+          <h2 className="text-slate-50 font-bold">Shares Outstanding</h2>
+          <p className="mt-1">
+            The shares outstanding is the number of shares that the company has
+          </p>
+        </div>
+      )}
       <div className="mt-3">
-        <h2 className="text-slate-50 font-bold">Preferred Stock Price</h2>
-        <p className="mt-1">
-          The preferred stock price is the price at which investors currently
-          pay for shares of the company. You can ask your recruiter what the
-          current price is.
+        {isoCurrent && (
+          <>
+            <h2 className="text-slate-50 font-bold">
+              Expected Growth over 4 years
+            </h2>
+            <p className="mt-1">
+              Depending on the stage of the company expected growth can vary.
+              Investors typically expect a 10x return on average, so you can put
+              a number between 5 to 15 here.
+            </p>
+          </>
+        )}
+        {rsuCurrent && (
+          <>
+            <h2 className="text-slate-50 font-bold">Expected Market Growth</h2>
+            <p className="mt-1"></p>
+          </>
+        )}
+        {(isoRevenue || rsuRevenue) && (
+          <>
+            <h2 className="text-slate-50 font-bold">
+              Expected Company Revenue
+            </h2>
+            <p className="mt-1"></p>
+            <h2 className="text-slate-50 font-bold mt-3">Revenue Multiple</h2>
+            <p className="mt-1"></p>
+          </>
+        )}
+        <p className="mt-3 italic">
+          Alternatively, you can estimate reasonable numbers by looking at
+          competitors:
         </p>
-      </div>
-      <div className="mt-3">
-        <h2 className="text-slate-50 font-bold">
-          Expected Growth over 4 years
-        </h2>
-        <p className="mt-1">
-          Depending on the stage of the company expected growth can vary.
-          Investors typically expect a 10x return on average, so you can put a
-          number between 5 to 15 here.
-        </p>
-        <p className="mt-2">
-          Alternatively, you can estimate a growth rate by looking at
-          competitors. The form below can help provide a reasonable estimate:
-        </p>
-        <p className="mt-4">
+        <form className="mt-2">
           <FormField
             className="rounded"
-            label="Add a competitor"
+            label="Add your company or a competitor"
             name="competitor"
             placeholder="Google"
           >
             <input />
           </FormField>
-        </p>
+        </form>
         <table className="relative min-w-full divide-y divide-slate-600 text-sm mt-2">
           <thead className="font-semibold">
             <tr>
               <th scope="col" className="px-3 py-3.5 text-left table-cell">
                 Company
               </th>
-              <th scope="col" className="px-3 py-3.5 text-left table-cell">
-                Base
-              </th>
-              <th scope="col" className="px-3 py-3.5 text-left table-cell">
-                Bonus
-              </th>
-              <th scope="col" className="px-3 py-3.5 text-left table-cell">
-                Stock
-              </th>
-              <th scope="col" className="px-3 py-3.5 text-right table-cell">
-                Total
-              </th>
+              {isoCurrent && (
+                <th scope="col" className="px-3 py-3.5 text-left table-cell">
+                  Growth over 4 years
+                </th>
+              )}
+              {rsuCurrent && (
+                <th scope="col" className="px-3 py-3.5 text-left table-cell">
+                  Current Market Value
+                </th>
+              )}
+              {rsuCurrent && (
+                <th scope="col" className="px-3 py-3.5 text-left table-cell">
+                  Average Growth per year
+                </th>
+              )}
+              {(isoRevenue || rsuRevenue) && (
+                <>
+                  <th scope="col" className="px-3 py-3.5 text-left table-cell">
+                    Shares Outstanding
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left table-cell">
+                    Revenue
+                  </th>
+                  <th scope="col" className="px-3 py-3.5 text-left table-cell">
+                    Revenue Multiple
+                  </th>
+                </>
+              )}
             </tr>
           </thead>
           <tbody className="text-slate-500">
